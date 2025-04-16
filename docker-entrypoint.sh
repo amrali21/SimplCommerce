f3afd7a8efaa9165ae40e04ef04a41944a9e4bc9
amrali21 <amoormoneer@gmail.com>
2025-04-16 18:00:31 +0200

    modify docker entry point file so api will  keep waiting for the db to respond

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 set -e
 
-if psql -h simpldb --username postgres -lqt | cut -d \| -f 1 | grep -qw simplcommerce; then
+# Wait until SQL Server is ready
+until psql -h postgres-db -U postgres -d mydb -c "SELECT 1" &> /dev/null; do
+  echo "Waiting for SQL Server..."
+  sleep 5
+done
+
+echo "NOW RUN BASH PROCESS"
+if psql -h postgres-db --username postgres -lqt | cut -d \| -f 1 | grep -qw simplcommerce; then
     echo "simplcommerce database existed"
 else
     echo "create new database simplcommerce"
-	psql -h simpldb --username postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
-	psql -h simpldb --username postgres -d simplcommerce -a -f /app/dbscript.sql
+    {
+    psql -h postgres-db --username postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
+    psql -h postgres-db --username postgres -d simplcommerce -a -f /app/dbscript.sql
+    } || {
+          echo "db error will skip"
+    }
 fi
 
 cd /app && dotnet SimplCommerce.WebHost.dll 
